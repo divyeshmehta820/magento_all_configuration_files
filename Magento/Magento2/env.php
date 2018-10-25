9f31b20fc1454614aed4bcc419f085b2a7dc2d67
Divyesh Mehta <divyesh.mehta@indianic.com>
2018-10-25 12:43:38 +0530

    local configuration files

---
+++ Magento/Magento2/env.php
@@ -0,0 +1,50 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => 'b52f57bcf622c026b468a78b18fd990a'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'dbname',
+                'username' => 'root',
+                'password' => 'password',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'full_page' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'translate' => 1,
+        'config_webservice' => 1,
+        'compiled_config' => 1
+    ],
+    'install' => [
+        'date' => 'Fri, 06 Jul 2018 10:57:10 +0000'
+    ]
+];
